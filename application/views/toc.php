59fac741fa87042965163486e56c0477723e529f
Jeff Verkoeyen <jverkoey@gmail.com>
2010-04-22 21:29:17 -0700

    Move toc to a common file.

---
+++ application/views/toc.php
@@ -0,0 +1,54 @@
+<div id="toc">
+  <div class="header">Getting Started</div>
+  <ul>
+    <li><a href="/overview">Three20 Overview</a></li>
+    <li><a href="/setup/existing">Add it to your project</a></li>
+    <li><a href="/setup/templates">Create projects with it</a></li>
+    <li><a href="http://scope.three20.info/">Three20 Scope</a></li>
+  </ul>
+
+  <div class="header">Tutorials</div>
+  <ul>
+    <li><a href="/tutorials/github">Build a GitHub App</a></li>
+  </ul>
+
+  <div class="header">Core</div>
+  <ul>
+    <li><a href="/core/debugging">Xcode debug++</a></li>
+  </ul>
+
+  <div class="header">Style</div>
+  <ul>
+    <li><a href="/style/overview">Overview</a></li>
+  </ul>
+
+  <div class="header">UI</div>
+  <ul>
+    <li><a href="/ui/navigation">URL-Based Navigation</a></li>
+  </ul>
+
+  <div class="header">Three20 Timeline</div>
+  <ul>
+    <li><a href="/timeline/roadmap">Roadmap</a></li>
+  </ul>
+
+  <div class="advertisement">
+  <!-- Beginning of Project Wonderful ad code: -->
+  <!-- Ad box ID: 46488 -->
+  <script type="text/javascript">
+  <!--
+  var pw_d=document;
+  pw_d.projectwonderful_adbox_id = "46488";
+  pw_d.projectwonderful_adbox_type = "4";
+  pw_d.projectwonderful_foreground_color = "";
+  pw_d.projectwonderful_background_color = "";
+  //-->
+  </script>
+  <script type="text/javascript" src="http://www.projectwonderful.com/ad_display.js"></script>
+  <noscript><map name="admap46488" id="admap46488"><area href="http://www.projectwonderful.com/out_nojs.php?r=0&amp;c=0&amp;id=46488&amp;type=4" shape="rect" coords="0,0,125,125" title="" alt="" target="_blank" /></map>
+  <table cellpadding="0" border="0" cellspacing="0" width="125" bgcolor="#ffffff"><tr><td><img src="http://www.projectwonderful.com/nojs.php?id=46488&amp;type=4" width="125" height="125" usemap="#admap46488" border="0" alt="" /></td></tr><tr><td bgcolor="#ffffff" colspan="1"><center><a style="font-size:10px;color:#0000ff;text-decoration:none;line-height:1.2;font-weight:bold;font-family:Tahoma, verdana,arial,helvetica,sans-serif;text-transform: none;letter-spacing:normal;text-shadow:none;white-space:normal;word-spacing:normal;" href="http://www.projectwonderful.com/advertisehere.php?id=46488&amp;type=4" target="_blank">Ads by Project Wonderful!  Your ad here, right now: $0</a></center></td></tr></table>
+  </noscript>
+  <!-- End of Project Wonderful ad code. -->
+  </div>
+
+</div> <!-- toc -->
