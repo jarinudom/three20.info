b4dcb18042f8b56e07f78ad8cc51df62cc0f3e03
Jeff Verkoeyen <jverkoey@gmail.com>
2010-05-26 03:47:45 -0400

    Flesh out the gallery.

--- application/views/gallery.php
+++ application/views/gallery.php
@@ -1,7 +1,177 @@
 The Three20 Gallery
 ===================
 
-This is a showcase of some apps currently in the App Store that use Three20.
+This is a showcase of some apps currently in the App Store that use Three20, shown in
+alphabetical order.
+
+
+CityTag
+-------
+
+<div class="source">Created by: Appiphany Pty Ltd.</div>
+
+<div class="appstore" markdown=1><a href="http://itunes.apple.com/au/app/citytag/id363504851">![FotoFind](/gfx/appstore.png 230x80)</a></div>
+
+CityTag is a geospatial iPhone application that connects the user with public amenities, and
+places of interest in their local community. This includes parks, playgrounds, gardens, cafes,
+businesses, toilets, change tables, parking or anything else that has a location. The application
+will provide directions to the location, and allows the user to add new items, annotate photos,
+post comments and manage out of date information.
+
+### Links
+
+* [Website](http://citytag.com.au/)
+
+### Three20 Components
+
+* [TTStyle](http://api.three20.info/interface_t_t_style.html)
+* [TTImageView](http://api.three20.info/interface_t_t_image_view.html)
+* [TTPhotoViewController](http://api.three20.info/interface_t_t_photo_view_controller.html)
+
+### Screenshots
+
+<span class="image darker" markdown=1>![CityTag](/gfx/gallery/citytag/screenshot0.jpg 320x460)</span>
+<span class="image darker" markdown=1>![CityTag](/gfx/gallery/citytag/screenshot1.jpg 320x460)</span>
+
+
+
+The Color of War
+----------------
+
+<div class="source">Created by: David Ethell</div>
+
+<div class="appstore" markdown=1><a href="http://itunes.apple.com/us/app/the-color-of-war/id360775514?mt=8">![FotoFind](/gfx/appstore.png 230x80)</a></div>
+
+The Color of War is a photo viewing application for browsing color photographs from the Second
+World War. Each photo includes detailed historical captions. Most of the photos were taken "on the
+ground" by amateurs with the remaining being official War Department photos. This is a free app
+providing a sampling of photos with more photos downloadable through in-app purchase.
+
+### Links
+
+* [Website](http://ww2color.com/iphone/)
+
+### Three20 Components
+
+* [TTImageView](http://api.three20.info/interface_t_t_image_view.html)
+* [TTLauncherView](http://api.three20.info/interface_t_t_launcher_view.html)
+* [TTNavigator](/ui/navigation)
+* [TTPhotoViewController](http://api.three20.info/interface_t_t_photo_view_controller.html)
+* [TTThumbsViewController](http://api.three20.info/interface_t_t_thumbs_view_controller.html)
+
+### Screenshots
+
+<span class="image darker" markdown=1>![The Color of War](/gfx/gallery/colorofwar/screenshot0.jpg 320x480)</span>
+<span class="image darker" markdown=1>![The Color of War](/gfx/gallery/colorofwar/screenshot1.jpg 320x480)</span>
+
+
+
+FotoFind
+--------
+
+<div class="source">Created by: Bethany Associates Inc.</div>
+
+<div class="appstore" markdown=1><a href="http://itunes.apple.com/us/app/fotofind/id371131067?mt=8">![FotoFind](/gfx/appstore.png 230x80)</a></div>
+
+FotoFind transforms a classic memory game into a fun and engaging photo discovery adventure.
+Enjoy trying to match photos from a set of cards on a beautifully textured wood table. Watch the
+cards animate as you discover new photos and explore where they were taken on a world map.
+
+### Links
+
+* [Website](http://www.fotofindapp.com)
+
+### Three20 Components
+
+* [TTPhotoViewController](http://api.three20.info/interface_t_t_photo_view_controller.html)
+* [TTMarkupStripper](http://api.three20.info/interface_t_t_markup_stripper.html)
+
+### Screenshots
+
+<span class="image darker" markdown=1>![FotoFind](/gfx/gallery/fotofind/screenshot0.jpg 320x480)</span>
+<span class="image darker" markdown=1>![FotoFind](/gfx/gallery/fotofind/screenshot1.jpg 320x480)</span>
+
+
+
+Goodreads
+---------
+
+<div class="source">Created by: Goodreads</div>
+
+<div class="appstore" markdown=1><a href="http://itunes.apple.com/us/app/goodreads/id355833469?mt=8">![FotoFind](/gfx/appstore.png 230x80)</a></div>
+
+Goodreads members recommend books, compare what they are reading, keep track of what they've read
+and would like to.
+
+### Links
+
+* [Website](http://www.brushthedog.com/)
+
+### Three20 Components
+
+* [TTNavigator](/ui/navigation)
+* [TTStyle](http://api.three20.info/interface_t_t_style.html)
+* [TTStyleTextLabel](http://api.three20.info/interface_t_t_styled_text_label.html)
+* [TTTables](http://api.three20.info/interface_t_t_table_view_controller.html)
+* [TTThumbsViewController](http://api.three20.info/interface_t_t_thumbs_view_controller.html)
+* [TTWebController](http://api.three20.info/interface_t_t_web_controller.html)
+
+### Screenshots
+
+<span class="image darker" markdown=1>![Goodreads](/gfx/gallery/goodreads/screenshot0.jpg 320x480)</span>
+<span class="image darker" markdown=1>![Goodreads](/gfx/gallery/goodreads/screenshot1.jpg 320x480)</span>
+
+
+
+Hacker News
+-----------
+
+<div class="source">Created by: Michael Grinich</div>
+
+<div class="appstore" markdown=1><a href="http://itunes.apple.com/ca/app/hacker-news/id339408916?mt=8">![Hacker News](/gfx/appstore.png 230x80)</a></div>
+
+A native Hacker News reader for iPhone.
+
+### Links
+
+* [Website](http://michaelgrinich.com/hackernews)
+
+### Three20 Components
+
+* [TTNavigator](/ui/navigation)
+* [TTURLRequest](http://api.three20.info/interface_t_t_u_r_l_request.html)
+* [TTTables](http://api.three20.info/interface_t_t_table_view_controller.html)
+* [TTStyle](http://api.three20.info/interface_t_t_style.html)
+
+### Screenshots
+
+<span class="image darker" markdown=1>![Hacker News](/gfx/gallery/hackernews/screenshot0.jpg 320x480)</span>
+<span class="image darker" markdown=1>![Hacker News](/gfx/gallery/hackernews/screenshot1.jpg 320x480)</span>
+
+
+Pompidou
+--------
+
+<div class="source">Created by: TAC Creative</div>
+
+<div class="appstore" markdown=1><a href="http://itunes.apple.com/fr/app/pompidou/id372939642?mt=8#">![Pompidou](/gfx/appstore.png 230x80)</a></div>
+
+Official application for the Centre Pompidou (Museum, France).
+
+### Three20 Components
+
+* [TTNavigator](/ui/navigation)
+* [TTImageView](http://api.three20.info/interface_t_t_image_view.html)
+* [TTPhotoViewController](http://api.three20.info/interface_t_t_photo_view_controller.html)
+* [TTTables](http://api.three20.info/interface_t_t_table_view_controller.html)
+* [TTScrollView](http://api.three20.info/interface_t_t_scroll_view.html)
+* [TTURLCache](http://api.three20.info/interface_t_t_u_r_l_cache.html)
+
+### Screenshots
+
+<span class="image darker" markdown=1>![Pompidou](/gfx/gallery/pompidou/screenshot0.jpg 320x460)</span>
+<span class="image darker" markdown=1>![Pompidou](/gfx/gallery/pompidou/screenshot1.jpg 320x460)</span>
+
 
 UH Guide
 --------
@@ -20,12 +190,11 @@ The UH Guide iPhone application assists you in finding your way around the Unive
 ### Three20 Components
 
 * [TTLauncherView](http://api.three20.info/interface_t_t_launcher_view.html)
-* [TTOpenURL](/ui/navigation)
+* [TTNavigator](/ui/navigation)
 * [TTURLRequest](http://api.three20.info/interface_t_t_u_r_l_request.html): caching for our "points of interest" requests
 * [TTPhotoViewController](http://api.three20.info/interface_t_t_photo_view_controller.html): nice thumbnail photos from Flickr
 * [TTTabBar](http://api.three20.info/interface_t_t_tab_bar.html): switching between content
-* [TTTable](http://api.three20.info/interface_t_t_table_view_controller.html): tweets
-
+* [TTTables](http://api.three20.info/interface_t_t_table_view_controller.html): tweets
 
 ### Screenshots
 
@@ -34,23 +203,3 @@ The UH Guide iPhone application assists you in finding your way around the Unive
 <span class="image darker" markdown=1>![UH Guide](/gfx/gallery/uhguide/screenshot2.jpg 320x480)</span>
 <span class="image darker" markdown=1>![UH Guide](/gfx/gallery/uhguide/screenshot3.jpg 320x480)</span>
 <span class="image darker" markdown=1>![UH Guide](/gfx/gallery/uhguide/screenshot4.jpg 320x480)</span>
-
-Hacker News
------------
-
-<div class="source">Created by: Michael Grinich.</div>
-
-<div class="appstore" markdown=1><a href="http://itunes.apple.com/ca/app/hacker-news/id339408916?mt=8">![UH Guide](/gfx/appstore.png 230x80)</a></div>
-
-A native Hacker News reader for iPhone.
-
-### Links
-
-* [Website](http://michaelgrinich.com/hackernews)
-
-### Three20 Components
-
-* [TTOpenURL](/ui/navigation)
-* [TTURLRequest](http://api.three20.info/interface_t_t_u_r_l_request.html)
-* [TTTable](http://api.three20.info/interface_t_t_table_view_controller.html)
-* [TTStyle](http://api.three20.info/interface_t_t_style.html)
